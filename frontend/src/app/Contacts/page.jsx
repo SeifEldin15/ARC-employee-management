08ef31e53e5fcbf42873350da6fc0ce4bb9a48d1
seifeldin15 <seifeldin02@outlook.com>
2025-01-10 13:47:53 +0200

    contacts page

---
+++ frontend/src/app/Contacts/page.jsx
@@ -0,0 +1,11 @@
+'use client';
+import Sidebar from '@/components/Sidebar';
+
+export default function DashboardPage() {
+  return (
+    <div className="min-h-screen">
+      <Sidebar />
+     
+    </div>
+  );
+}

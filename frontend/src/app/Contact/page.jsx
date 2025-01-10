ed642795267b2206f2f67da572152b0581ebfe9f
seifeldin15 <seifeldin02@outlook.com>
2025-01-10 16:03:50 +0200

    contact page

---
+++ frontend/src/app/Contact/page.jsx
@@ -0,0 +1,39 @@
+'use client';
+import Sidebar from '@/components/Sidebar';
+import LocationSection from '@/components/Contact/LocationSection';
+import Link from 'next/link';
+import ContactSection from '@/components/Contact/ContactSection';
+import Table from '@/components/Contact/Table';
+
+export default function DashboardPage() {
+  return (
+    <div className="min-h-screen">
+      <Sidebar />
+      <div className="md:ml-64 flex-1">
+        <div className="md:p-8 p-4 pt-20 md:pt-8">
+          {/* Back button container */}
+          <div className="flex justify-center mb-8">
+            <Link 
+              href="/contacts" 
+              className="text-gray-600 hover:text-gray-800 inline-block text-base border border-gray-200 rounded px-4 py-2"
+            >
+              ← Back to Contacts
+            </Link>
+          </div>
+
+          {/* Company header */}
+          <div className="text-center mb-8 max-w-4xl mx-auto">
+            <div className=" rounded-lg ">
+              <h1 className="text-3xl font-semibold text-gray-800">Acme Technologies</h1>
+              <p className="text-gray-700 mt-2 bg-gray-200 ">#738863</p>
+            </div>
+          </div>
+
+          <LocationSection />
+          <ContactSection />
+          <Table />
+        </div>
+      </div>
+    </div>
+  );
+}

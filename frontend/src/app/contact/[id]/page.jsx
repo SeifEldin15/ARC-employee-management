d5b1dea75832017effb97b4fca5b733b91fb0cc7
seifeldin15 <seifeldin02@outlook.com>
2025-01-12 19:06:19 +0200

    id folder to handle dynamic companies

--- frontend/src/app/contact/[id]/page.jsx
+++ frontend/src/app/contact/[id]/page.jsx
@@ -1,4 +1,5 @@
 'use client';
+import { useParams } from 'next/navigation';
 import Sidebar from '@/components/Sidebar';
 import LocationSection from '@/components/Contact/LocationSection';
 import Link from 'next/link';
@@ -6,6 +7,9 @@ import ContactSection from '@/components/Contact/ContactSection';
 import Table from '@/components/Contact/Table';
 
 export default function DashboardPage() {
+  const params = useParams();
+  const id = params.id; // This will give you access to the dynamic ID parameter
+
   return (
     <div className="min-h-screen">
       <Sidebar />
@@ -25,7 +29,7 @@ export default function DashboardPage() {
           <div className="text-center mb-8 max-w-4xl mx-auto">
             <div className=" rounded-lg ">
               <h1 className="text-3xl font-semibold text-gray-800">Acme Technologies</h1>
-              <p className="text-gray-700 mt-2 bg-gray-200 ">#738863</p>
+              <p className="text-gray-700 mt-2 bg-gray-200 ">{id}</p>
             </div>
           </div>
 

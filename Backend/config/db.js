10d6227e0f89c7816f03aa8e64c9ed0d97dc43c1
seifeldin15 <seifeldin02@outlook.com>
2025-01-13 02:45:30 +0200

    changed db uri

--- Backend/config/db.js
+++ Backend/config/db.js
@@ -1,16 +1,13 @@
 import mongoose from 'mongoose';
-import { MONGO_URI } from './env.js'
+import { MONGO_URI } from '../env.js';
 
 const connectDB = async () => {
     try {
-        console.log(MONGO_URI)
-        await mongoose.connect(MONGO_URI, {
-            useNewUrlParser: true,
-            useUnifiedTopology: true,
-        });
+        console.log('Attempting to connect with URI:', MONGO_URI);
+        const conn = await mongoose.connect(MONGO_URI);
         console.log('MongoDB connected');
     } catch (error) {
-        console.error('MongoDB connection failed:', error.message);
+        console.error('Error connecting to MongoDB:', error.message);
         process.exit(1);
     }
 };

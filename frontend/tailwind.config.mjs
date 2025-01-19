12051ae21ebeacadd360ac17c96c93d9b87d1375
seifeldin15 <seifeldin02@outlook.com>
2025-01-19 17:56:10 +0200

    new color

--- frontend/tailwind.config.mjs
+++ frontend/tailwind.config.mjs
@@ -11,6 +11,9 @@ export default {
         background: "var(--background)",
         foreground: "var(--foreground)",
       },
+      gridTemplateColumns: {
+        '52': 'repeat(52, minmax(0, 1fr))',
+      },
     },
   },
   plugins: [],
